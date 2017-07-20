0b5ee8a380a5987bdd39440e3a52efc8c61fcb87
Adam Kazberuk <adam.kazberuk@narvar.com>
2017-07-20 10:18:00 -0700

    i'm good at forgetting commas

--- utility/temperature.js
+++ utility/temperature.js
@@ -1,10 +1,10 @@
 var temperatureUtility = {
     convertKelvinToF: function convertKelvinToF(kelvinTemp){
       return (9/5) * kelvinTemp - 459.67;
-    }
+    },
     convertKelvinToC: function convertKelvinToC(kelvinTemp){
       return kelvinTemp - 273.15;
-    }
+    },
     convertKelvin: function convertKelvin(kelvinTemp){
       var convertedArr = [];
       for(var i=0; i<arguments.length; i++){
