1ef3bfc8ad79966ae785e40175935fb76314e9bf
Adam Kazberuk <adam.kazberuk@narvar.com>
2017-07-20 10:25:26 -0700

    a

--- utility/temperature.js
+++ utility/temperature.js
@@ -1,20 +1,25 @@
-var temperatureUtility = {
-    convertKelvinToF: function convertKelvinToF(kelvinTemp){
-      return (9/5) * kelvinTemp - 459.67;
-    },
-    convertKelvinToC: function convertKelvinToC(kelvinTemp){
-      return kelvinTemp - 273.15;
-    },
-    convertKelvin: function convertKelvin(kelvinTemp){
-      var convertedArr = [];
-      for(var i=0; i<arguments.length; i++){
-        convertedArr.push({
-          f: convertKelvinToF(arguments[i]),
-          c: convertKelvinToC(arguments[i])
-        })
-      }
-      return convertedArr;
+function temperatureUtility(){
+  var self = {};
+  self.convertKelvinToF = function convertKelvinToF(kelvinTemp){
+    return (9/5) * kelvinTemp - 459.67;
+  };
+
+  self.convertKelvinToC = function convertKelvinToC(kelvinTemp){
+    return kelvinTemp - 273.15;
+  };
+
+  self.convertKelvin = function convertKelvin(kelvinTemp){
+    var convertedArr = [];
+    for(var i=0; i<arguments.length; i++){
+      convertedArr.push({
+        f: convertKelvinToF(arguments[i]),
+        c: convertKelvinToC(arguments[i])
+      })
     }
+    return convertedArr;
+  }
+
+  return self;
 }
 
-module.exports = temperatureUtility;
+module.exports = temperatureUtility();
